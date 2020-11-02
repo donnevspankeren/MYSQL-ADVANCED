83f9bacc7d09eb046774355a75188bea1edaf2df
donnevspankeren <donne.van.spankeren01@gmail.com>
2020-11-02 15:48:39 +0100

    taak 2 insert

--- 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
+++ 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
@@ -1,1 +1,12 @@
-
+1: INSERT INTO `studenten`(`voornaam`, `achternaam`, `klas`, `adres`, `postcode`, `geboortedatum`, `woonplaats`) 
+VALUES( 'Berta','Beenstra','LCTAOO9A','Beethovenlaan','1233KU','2002-11-25','De Kwakel');
+2: INSERT INTO `studenten`(`voornaam`, `achternaam`, `klas`, `adres`, `postcode`, `geboortedatum`, `woonplaats`) 
+VALUES( 'hugo','elgo','LCTAOO9A','Kruiswijk','1467IO','2002-05-21','Kudelstaart');
+3:INSERT INTO `studenten`(`voornaam`, `achternaam`, `klas`, `adres`, `postcode`, `geboortedatum`, `woonplaats`) 
+VALUES( 'ernst','weert','LCTAOO9B','Drommedarislaan','1643DF','2002-9-27','Aalsmeer');
+4: INSERT INTO `studenten`(`voornaam`, `achternaam`,`woonplaats`) 
+VALUES ('Egbert','Veenstra','Amsterdam')
+5: INSERT INTO `studenten`(`voornaam`, `achternaam`,`woonplaats`) 
+VALUES ('Marit','Herfstui','Hoofddorp')
+6: INSERT INTO `studenten`(`voornaam`, `achternaam`,`woonplaats`) 
+VALUES ('Sophie','kaars','Amstelveen')
