cd9944dcab80501ef411131533d8e7c73cd317d6
donnevspankeren <donne.van.spankeren01@gmail.com>
2020-11-02 15:30:21 +0100

    taak 1 insert

--- 1-INSERT/taak01 - Insert Into/antwoorden.sql
+++ 1-INSERT/taak01 - Insert Into/antwoorden.sql
@@ -1,1 +1,12 @@
-
+1: INSERT INTO `studenten`(`student_id`, `voornaam`, `achternaam`, `klas`, `adres`, `postcode`, `geboortedatum`, `woonplaats`) 
+VALUES (7,'Donne','van Spankeren','LCTAOO9A','Irislaan','1424CM','2002-09-07','De Kwakel');
+2:INSERT INTO `studenten`(`student_id`, `voornaam`, `achternaam`, `klas`, `adres`, `postcode`, `geboortedatum`, `woonplaats`) 
+VALUES (8,'Gwenn','Pesch','LCTAOO9B','Amperestraat','1433KV','2002-03-14','Kudelstraat');
+3:INSERT INTO `studenten`(`student_id`, `voornaam`, `achternaam`, `klas`, `adres`, `postcode`, `geboortedatum`, `woonplaats`) 
+VALUES (9,'KLaas','Veenstra','LCTAOO9A','Leendrolaan','1554DL','2002-06-08','Uithoorn');
+4:INSERT INTO `studenten`(`student_id`, `voornaam`, `achternaam`, `klas`, `adres`, `postcode`, `geboortedatum`, `woonplaats`) 
+VALUES (10,'Maartje','van Dijnksa','LCTAOO9B','Roosstraat','1435LK','2002-12-07','Kudelstraat');
+5:INSERT INTO `studenten`(`student_id`, `voornaam`, `achternaam`, `klas`, `adres`, `postcode`, `geboortedatum`, `woonplaats`) 
+VALUES (11,'Berend','Groenenhof','LCTAOO9C','driehoeklaan','1788UI','2003-01-30','Aaslmeer');
+6:INSERT INTO `studenten`(`student_id`, `voornaam`, `achternaam`, `klas`, `adres`, `postcode`, `geboortedatum`, `woonplaats`) 
+VALUES (12,'Rachel','Uitersta','LCTAOO9C','Bloemengracht','1477RS','2002-05-23','De Kwakel');
